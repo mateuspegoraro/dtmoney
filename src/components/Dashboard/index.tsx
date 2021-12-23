935fc1ccf025fa75d5a00700c68786e6ab593f66
Mateus Pegoraro <mateus.pegoraro@involves.com>
2021-12-23 08:26:48 -0300

    Componente: TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -2,12 +2,14 @@
 import { Container } from './styles';
 
 import { Summary } from '../Summary'
+import { TransactionsTable } from '../TransactionsTable'
 
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 };
